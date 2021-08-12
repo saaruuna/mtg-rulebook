6822e1ff1daac8347149a535f9daff2400ef061a
saaruuna <uuna.saarela@gmail.com>
2021-08-12 23:46:45 +0300

    env fix

--- mtg_rulebook/settings.py
+++ mtg_rulebook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', 'sample_secret')
+SECRET_KEY = env.str('SECRET_KEY', 'sample_unsafe_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
