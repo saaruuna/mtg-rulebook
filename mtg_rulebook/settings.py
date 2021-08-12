d63f96d5f81081705b4eae39c32d1ea6644db428
saaruuna <uuna.saarela@gmail.com>
2021-08-13 00:27:10 +0300

    static fix

--- mtg_rulebook/settings.py
+++ mtg_rulebook/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT   =   os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'rules/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
